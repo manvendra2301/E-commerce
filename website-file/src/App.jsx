1b57b962dfda1106594f150662b2a3ac3fed37a8
manvendra2301 <manvendrapathya01@gmail.com>
2024-08-16 01:15:59 +0530

    initial commit

---
+++ website-file/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+import { Navbar } from './Components/Navbar/Navbar'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
