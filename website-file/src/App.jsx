4b9584ecec3a1d1f1624db0c0d3f67c7671f0815
manvendra2301 <manvendrapathya01@gmail.com>
2024-08-19 01:41:18 +0530

    addded context api and listed products of various category

--- website-file/src/App.jsx
+++ website-file/src/App.jsx
@@ -3,12 +3,16 @@ import './App.css'
 
 import { Navbar } from './Components/Navbar/Navbar'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { ShopCategory } from './Pages/ShopCategory';
+import ShopCategory  from './Pages/ShopCategory';
 import Shop from './Pages/Shop'
 import { Product } from './Pages/Product';
 import { Cart } from './Pages/Cart';
 import { LoginSignup } from './Pages/LoginSignup';
 import { Footer } from './Components/Footer/Footer';
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kid_banner from './Components/Assets/banner_kids.png'
+
 
 
 
@@ -20,9 +24,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="men" />} />
-          <Route path="/women " element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kid" />} />
+          <Route path="/mens" element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path="/womens" element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path="/kids" element={<ShopCategory banner={kid_banner}category="kid" />} />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
---
+++ website-file/src/Context/ShopContext.jsx
@@ -0,0 +1,16 @@
+import React, {createContext} from "react";
+import all_products from '../Components/Assets/all_product'
+
+export const ShopContext = createContext(null);
+
+const ShopContextProvider =(props) => {
+  const contextValue = {all_products};
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  )
+}
+
+export default ShopContextProvider;
