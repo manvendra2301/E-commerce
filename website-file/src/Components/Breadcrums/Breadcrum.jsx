a2f73eaec34953a2fd6a853eaf06b88753b5eaaf
manvendra2301 <manvendrapathya01@gmail.com>
2024-08-20 01:11:17 +0530

    created login-signup page

---
+++ website-file/src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Breadcrum.css'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
+
+export const Breadcrum = (props) => {
+  const {product} = props;
+    return (
+    <div className='breadcrum'>
+
+    </div>
+  )
+}
--- website-file/src/Pages/Product.jsx
+++ website-file/src/Pages/Product.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import { useParams } from 'react-router-dom';
 
 export const Product = () => {
+  const {all_products} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_products.find((e)=> e.id === Number(productId))
   return (
     <div></div>
   )
